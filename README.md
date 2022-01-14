77dfb4b4975a5b93cf21303ef77e368abf4c36d4
Eiichi <8284623+eiichi-worker@users.noreply.github.com>
2022-01-15 00:29:52 +0900

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+washira.co
+===
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d2abaa37-0b43-42cb-a0bb-2de6b5d09375/deploy-status)](https://app.netlify.com/sites/epic-turing-9097ff/deploys)
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+const ctx = useRuntimeConfig();
+
+const { data } = await useFetch("/blog", {
+  baseURL: ctx.baseURL,
+  headers: {
+    "X-MICROCMS-API-KEY": ctx.apiKey,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>合同会社わしら<small>（仮）</small></h1>
+  </div>
+</template>
